f98e9638dc9225bb03a2784f88c30409a536a94a
raytp29-hub <spa.giaio981@gmail.com>
2025-08-19 02:47:17 +0200

    test base

--- app.py
+++ app.py
@@ -1,7 +1,25 @@
 import streamlit as st
 import pandas as pd
 
-st.title("Big Ambition Analyzer")
-st.write("Work in progress...")
+st.set_page_config(
+    page_title="Big Abmitions Analyzer",
+    page_icon="🎮",
+    layout="wide"
+)
 
 
+st.title("🎮 Big Ambitions Business Analyzer")
+st.write("Analize you business data from Big Ambitions")
+
+
+#Upload file 
+uploaded_file = st.file_uploader(
+    "upload your csv file",
+    type=["csv"],
+    help="Export file from your game and upload here"
+)
+
+if uploaded_file is not None:
+    st.success("File uploaded")
+else:
+    st.info("Upload file for start analysis")
